9aba8dbcb68791920b3058113a393ab37e846194
iwint <iwint.cseasnsct@gmail.com>
2024-05-25 17:08:33 +0530

    fix: checking

--- app/components/buttons/button.tsx
+++ app/components/buttons/button.tsx
@@ -21,23 +21,24 @@ const Button: React.FC<ButtonProps> = ({
   small,
 }) => {
   return (
-    <button
-      type="button"
-      className={`relative disabled:opacity-70 disabled:cursor-not-allowed rounded-lg hover:opacity-80 transition w-full ${
-        outline ? "bg-white" : "bg-rose-500"
-      } 
-      ${outline ? "border-black" : "border-rose-500"}
-      ${outline ? "text-black" : "text-white"}   
-      ${small ? "py-1" : "py-3"}
-      ${small ? "text-sm" : "text-md"}
-      ${small ? "font-light border-[1px]" : "font-semibold border-2"}
-      `}
-      disabled={disabled}
-      onClick={onClick}
-    >
-      {Icon && <Icon size={24} className="absolute left-4 top-3" />}
-      {label}
-    </button>
+    <div></div>
+    // <button
+    //   type="button"
+    //   className={`relative disabled:opacity-70 disabled:cursor-not-allowed rounded-lg hover:opacity-80 transition w-full ${
+    //     outline ? "bg-white" : "bg-rose-500"
+    //   }
+    //   ${outline ? "border-black" : "border-rose-500"}
+    //   ${outline ? "text-black" : "text-white"}
+    //   ${small ? "py-1" : "py-3"}
+    //   ${small ? "text-sm" : "text-md"}
+    //   ${small ? "font-light border-[1px]" : "font-semibold border-2"}
+    //   `}
+    //   disabled={disabled}
+    //   onClick={onClick}
+    // >
+    //   {Icon && <Icon size={24} className="absolute left-4 top-3" />}
+    //   {label}
+    // </button>
   );
 };
 
