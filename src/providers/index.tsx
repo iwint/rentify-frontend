5e495cf4427c658d5e32108ba94a645c5c90dd56
iwint <iwint.cseasnsct@gmail.com>
2024-05-29 16:58:59 +0530

    feat: changed loader for only listings

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,18 +1,14 @@
-import { ProviderProps } from "./providers-type";
+import { ThemeProvider as MaterialThemeProvider } from "@emotion/react";
 import { ThemeProvider as TailwindThemeProvider } from "@material-tailwind/react";
+import { ProviderProps } from "./providers-type";
 import { QueryProvider } from "./query-provider";
 import { ToastProvider } from "./toast-provider";
-import { ThemeProvider as MaterialThemeProvider } from "@emotion/react";
-import Loader from "@/components/common/loader";
-import { usePromiseTracker } from "react-promise-tracker";
 
 export const Provider = ({ children }: ProviderProps) => {
-  const { promiseInProgress } = usePromiseTracker();
   return (
     <QueryProvider>
       <TailwindThemeProvider>
         <MaterialThemeProvider theme={{}}>
-          <Loader isLoading={promiseInProgress} />
           <ToastProvider />
           {children}
         </MaterialThemeProvider>
